4d96d0c10d991c670bc75b7b70bafbae5a95c641
Pieter <13552343+pmoris@users.noreply.github.com>
2018-05-31 13:23:49 +0200

    Adds logger to bash pipeline script.

--- src/scripts/herpesviridae-analysis.sh
+++ src/scripts/herpesviridae-analysis.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
+cd $(dirname "$0")
 cd ../..
 
-python src/scripts/pathogen_selection.py -i data/raw/ppi_data/ -n data/raw/taxdump/ -t 10292 -o data/interim/
+python src/scripts/pathogen_selection.py -i data/raw/ppi_data/ -n data/raw/taxdump/ -t 10292 -o data/interim/ | tee $(dirname "$0")/10292_output_pathogen_selection.log
 
-python src/scripts/filter_and_remap.py -i data/interim/10292/ppi_data/10292-ppi-merged.tsv -t data/interim/10292/taxonid -o data/interim/10292
-
+python src/scripts/filter_and_remap.py -i data/interim/10292/ppi_data/10292-ppi-merged.tsv -t data/interim/10292/taxonid -o data/interim/10292 2>&1 | tee $(dirname "$0")/10292_output_filter_and_remap.log
